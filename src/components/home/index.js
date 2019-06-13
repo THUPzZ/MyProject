5d6d50f94f0738e24b986ae93cf58a260420a8ac
theeraprased <tub.meemui@gmail.com>
2019-06-13 18:02:42 +0700

    change class component home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,47 +1,47 @@
-import React/*{Component}*/ from 'react'
+import React,{Component} from 'react'
 import { connect } from 'react-redux'
 //import { increment } from '../../actions/actions'
 
-// class Home extends Component {
-//   render(){
-//     return (
-//       <div>
+class Home extends Component {
+  render(){
+    return (
+      <div>
 
+      </div>
+    )
+  }
+}
+export default Home;
+// const HomeComponent = ({ message, counter, dispatch  }) => (
+//       <div>
+//       <div className="container">
+//       <div className="columns column is-12">
+//         <h1>Counter : {counter}</h1>
 //       </div>
-//     )
+
+//       <div className="buttons">
+//         <button className="button is-primary" onClick={() => dispatch(increment(1))} >+1</button>
+//         <button className="button is-link" onClick={() => dispatch(increment(2))}>+2</button>
+//         <button className="button is-info" onClick={() => dispatch(increment(3))}>+3</button>
+//       </div>
+
+//       <div className="buttons">
+//         <button className="button is-primary" onClick={() => dispatch(increment(-1))}>-1</button>
+//         <button className="button is-link" onClick={() => dispatch(increment(-2))}>-2</button>
+//         <button className="button is-info" onClick={() => dispatch(increment(-3))}>-3</button>
+//       </div>
+//     </div>
+// </div>
+  
+// )
+
+// const mapStateToProps = function(state) {
+//   return {
+//     message: 'This is message from mapStateToProps',
+//     counter: state.counters || 0 
+    
 //   }
 // }
 
-const HomeComponent = ({ message, counter, dispatch  }) => (
-      <div>
-      {/*<div className="container">
-      <div className="columns column is-12">
-        <h1>Counter : {counter}</h1>
-      </div>
-
-      <div className="buttons">
-        <button className="button is-primary" onClick={() => dispatch(increment(1))} >+1</button>
-        <button className="button is-link" onClick={() => dispatch(increment(2))}>+2</button>
-        <button className="button is-info" onClick={() => dispatch(increment(3))}>+3</button>
-      </div>
-
-      <div className="buttons">
-        <button className="button is-primary" onClick={() => dispatch(increment(-1))}>-1</button>
-        <button className="button is-link" onClick={() => dispatch(increment(-2))}>-2</button>
-        <button className="button is-info" onClick={() => dispatch(increment(-3))}>-3</button>
-      </div>
-    </div>*/}
-</div>
-  
-)
-
-const mapStateToProps = function(state) {
-  return {
-    message: 'This is message from mapStateToProps',
-    counter: state.counters || 0 
-    
-  }
-}
-
-const Home = connect(mapStateToProps)(HomeComponent)
-export default Home
+// const Home = connect(mapStateToProps)(HomeComponent)
+// export default Home
